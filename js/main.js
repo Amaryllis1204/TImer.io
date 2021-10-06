6f740355fdb217ce8ee4ec051a6174585870da94
amaryllis1204 <amaryllis1204@gmail.com>
2021-10-06 22:26:09 +0900

    再開しても時間が0にならないバグの修正

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 let start;
 let timer_id;
+let stop = 0;
 
 //ボタン操作
 document.getElementById('start_stop').addEventListener('click', function(){
@@ -13,6 +14,7 @@ if(this.innerHTML === 'START'){
     this.classList.remove('btn-primary');
     this.classList.add('btn-danger');
 }else{
+    stop = goTimer();
     clearInterval(timer_id);
 
     //STARTボタンにする
@@ -35,9 +37,9 @@ return value;
 };
 
 //タイマー処理の関数
-let goTimer = function(){
+let goTimer = function(value){
 let now = new Date();
-let milli = now.getTime() - start.getTime();
+let milli = now.getTime() - start.getTime() + stop;
 let seconds = Math.floor(milli / 1000);
 let minutes = Math.floor(seconds / 60);
 let hours = Math.floor(minutes / 60);
@@ -50,4 +52,6 @@ minutes = addZero(minutes);
 hours = addZero(hours);
 
 document.getElementById('timer').innerHTML = hours + ':' + minutes + ':' + seconds;
+
+return milli;
 }
